d6415365bcfb360773c4777a69c9457c45afcf6e
zeeshan bashir <128677208+xeeshu-ai@users.noreply.github.com>
2026-06-04 21:38:28 +0530

    temp: reading file before full push

--- src/public/pages/TournamentDetails.jsx
+++ src/public/pages/TournamentDetails.jsx
@@ -1,1 +1,5 @@
-{content}
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import { supabasePlayer } from '../../lib/supabaseClient'
+import { usePlayer } from '../../lib/PlayerContext'
+import { useGame } from '../../lib/GameContext'
